d819b42d572fb8ce54edaeab3a409dffc6cd0da2
enzoftware <lizama.enzo@gmail.com>
2018-12-13 10:31:11 -0500

    :star:

--- app/src/main/java/com/projects/enzoftware/krazyposts/BaseActivity.kt
+++ app/src/main/java/com/projects/enzoftware/krazyposts/BaseActivity.kt
@@ -16,7 +16,7 @@ class BaseActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+            Snackbar.make(view, "Hello Rx", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
     }
